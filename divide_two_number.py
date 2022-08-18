04a0d7294eddb1b66c5d8c1d1bdf3e48e2d2535b
TammyTrinh <trinhthiminhtamvq@gmail.com>
2022-08-18 11:10:08 +0700

    Include ZeroDivisionError

--- divide_two_number.py
+++ divide_two_number.py
@@ -1,2 +1,10 @@
 def divide(a,b):
     return a/b 
+a,b=input("Enter 2 number: ").split()
+a=int(a)
+b=int(b)
+
+try:
+    divide(a,b)
+except ZeroDivisionError as err:
+    print('Handling run-time error:', err)
